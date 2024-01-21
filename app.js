6a0cd8e4543944c8476191952fc48bf04a55f226
dark258 <carlosescobarzarzar@gmail.com>
2024-01-21 19:21:16 -0300

    cambios segun clase 2

--- app.js
+++ app.js
@@ -4,5 +4,9 @@ let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 console.log(numeroUsuario);
 
 if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el número');
+    //alert('Acertaste el número secreto ' + numeroUsuario);
+    alert(`Acertaste el número secreto ${numeroUsuario}`);
+} else {
+    //alert('El número '+ numeroUsuario + ' no es el número secreto!')
+    alert(`El número ${numeroUsuario} no es el número secreto!`)
 }
