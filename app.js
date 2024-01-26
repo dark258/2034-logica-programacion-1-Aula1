4ba0ed5ead145ec63043f8b5d1c04dc2e38dfcca
dark258 <carlosescobarzarzar@gmail.com>
2024-01-26 18:01:59 -0300

    final edits

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+let numeroSecreto = parseInt(Math.random()*10+1);
+console.log(numeroSecreto + ' es el numero secreto');
 
-console.log(numeroUsuario);
-
-if (numeroUsuario == numeroSecreto) {
-    //alert('Acertaste el número secreto ' + numeroUsuario);
-    alert(`Acertaste el número secreto ${numeroUsuario}`);
-} else {
-    //alert('El número '+ numeroUsuario + ' no es el número secreto!')
-    alert(`El número ${numeroUsuario} no es el número secreto!`)
+let intentos = 3;
+while (intentos>0){
+    let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+    console.log(numeroUsuario + ' es el numero del usuario');
+    if (numeroUsuario == numeroSecreto) {
+        //alert('Acertaste el número secreto ' + numeroUsuario);
+        alert(`Acertaste el número secreto ${numeroUsuario}`);
+        break;
+    } else {
+        //alert('El número '+ numeroUsuario + ' no es el número secreto!')
+        alert(`El número ${numeroUsuario} no es el número secreto!`);
+    }
+    intentos--;
 }
+if (intentos == 0)
+alert('Excediste el máximo de intentos');
