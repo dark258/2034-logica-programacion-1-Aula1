533456a60dbce3b6990a0a3bb296ac3b449ff17d
dark258 <carlosescobarzarzar@gmail.com>
2024-01-26 18:25:25 -0300

    new game plus plus

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 let numeroSecreto = parseInt(Math.random()*10+1);
 let intentos = 3;
-while (intentos>0){
+while (intentos){
+    console.log(intentos)
+    let numeroUsuario = parseInt(prompt("Me indicas un número entre 1 y 10 por favor:"));
+    if (isNaN(numeroUsuario)){
+        alert('Debes ingresar un número!!');
+        continue;
+    }
     intentos--;
-    let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
     if (numeroUsuario == numeroSecreto) {
         alert(`Acertaste el número secreto ${numeroUsuario}`);
         break;
@@ -12,7 +17,7 @@ while (intentos>0){
         alert(`El número ${numeroUsuario} no es el número secreto! Su valor es mayor.`);
     }
 }
-if (intentos == 0)
+if (!intentos)
     alert('Excediste el máximo de intentos');
 else
     alert(`Acertaste al número secreto ${numeroSecreto} en ${3-intentos} intento${(3-intentos)==1?'':'s'}!`)
